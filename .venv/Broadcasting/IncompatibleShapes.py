968a877139d2f0cf6ec0b125cc8645f91bcea13b
N0tstgr <vikashkasaudhan321@gmail.com>
2026-01-31 07:06:35 +0530

    incompatible

--- .venv/Broadcasting/IncompatibleShapes.py
+++ .venv/Broadcasting/IncompatibleShapes.py
@@ -1,5 +1,9 @@
 import numpy as np
-m = np.array([40,29,20])
+# m = np.array([40,29,20])
+# n = np.array([22,56])
+m = np.array([[40,29,20],[33,67,89]])
 n = np.array([22,56])
+# reshape_m = m.reshape(2,2)
+# print(m)
 result = n+m
 print(result)
